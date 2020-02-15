88ce973871e68d45ae6ca93d3b69d8684d7aff59
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-02-15 01:08:50 +0100

    Fix reference removal in install process

--- .travis.sh
+++ .travis.sh
@@ -10,18 +10,18 @@ php --version
   curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 )
 
-runtime=$(hhvm --php -r "echo HHVM_VERSION[0] === '3' ? 'hhvm' : 'php';")
-if [ "$runtime" = "hhvm" ]; then
-  hhvm /usr/local/bin/composer install
-else
-  php /usr/local/bin/composer install
-fi
-
 use_polyfill=$(hhvm --php -r "echo HHVM_VERSION_ID < 42600 ? 'old' : 'new';")
 if [ "$use_polyfill" = "old" ]; then
   rm src/__Private/fb_intercept_polyfill_new.php
 else
   rm src/__Private/fb_intercept_polyfill_old.php
+fi
+
+runtime=$(hhvm --php -r "echo HHVM_VERSION[0] === '3' ? 'hhvm' : 'php';")
+if [ "$runtime" = "hhvm" ]; then
+  hhvm /usr/local/bin/composer install
+else
+  php /usr/local/bin/composer install
 fi
 
 hh_client
